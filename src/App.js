52a28dec4204caa3c9fdb462599f745223ecf6f5
Rizqy Fachri <rizqyfachri@gmail.com>
2023-03-27 11:31:09 +0700

    finish footer on mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "./App.css";
 import navbarLogo from "./img/re-logo.png";
 import erlan from "./img/foto-erlan.png";
+import bontel from "./img/foto-bontel.png";
 
 function App() {
+  // const element = <FontAwesomeIcon icon={faEnvelope} />;
   return (
     <div className="wrapper flex flex-col">
       <navbar>
@@ -81,7 +85,70 @@ function App() {
         <div className="work-section"></div>
       </main>
 
-      <footer></footer>
+      <footer>
+        <div className="footer bg-secondary flex flex-col md:flex-row py-10 md:py-30 px-5 md:px-20 md:items-center gap-y-8">
+          <section className="footer-left flex flex-col gap-y-2">
+            <h2 className="text-bodyOne text-primary pb-4">
+              About R&E Consultant
+            </h2>
+            <p className="text-bodyTwo text-black">
+              Okay it’s a little cringe. But we proudly say that R&E consultant
+              stands for Rizqy and Erlan Consultant. We don’t quite think very
+              much about our name. But we do think a lot about our value,
+              specially to you as our (future) client.
+            </p>
+            <p className="text-bodyTwo text-black">
+              We met at MBA Program, in Institut Teknologi Bandung and work from
+              that time to help our colleagues and friends to start and manage
+              their business. Now, we want to help more than our circle, by
+              doing this consultant shitttt. Also, we don’t have much money to
+              live.
+            </p>
+          </section>
+          <section className="footer-right border-2 border-primary rounded-lg">
+            <section className="rizqy-profile flex flex-col items-center gap-y-3 border-b-2 border-primary py-6">
+              <img
+                className="footer-picture w-36"
+                src={bontel}
+                alt="displayPicture"
+              ></img>
+              <h2 className="text-bodyOne text-primary">Rizqy Fachri</h2>
+              <p>Business consultant & product designer</p>
+              <a
+                role="button"
+                href="https://www.linkedin.com/in/rizqy-fachri/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  className="text-headingThree text-primary"
+                  icon={faLinkedin}
+                />
+              </a>
+            </section>
+            <section className="erlan-profile flex flex-col items-center gap-y-3 py-6">
+              <img
+                className="footer-picture w-36"
+                src={erlan}
+                alt="displayPicture"
+              ></img>
+              <h2 className="text-bodyOne text-primary">Erlan Saputra</h2>
+              <p>Business consultant & marketer</p>
+              <a
+                role="button"
+                href="https://www.linkedin.com/in/erlan-saputra-yudha/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FontAwesomeIcon
+                  className="text-headingThree text-primary"
+                  icon={faLinkedin}
+                />
+              </a>
+            </section>
+          </section>
+        </div>
+      </footer>
     </div>
   );
 }
